2e40989f45b8ce6d2f46133231c4f61b61347967
Gospel116 <dualedge4rock@gmail.com>
2023-06-17 07:48:53 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: This program prints a string as the output
+ * Return: o (success0
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
