f6c8f458ec3fa821186021f8f235cc42cd40acea
Gospel116 <dualedge4rock@gmail.com>
2023-06-21 20:21:16 -0700

    add two values

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - adds two values
+ * @n1: The first value
+ * @n2: The seconf value
+ * Return: Integer value
+ */
+
+int add(int n1, int n2)
+{
+	return (n1 + n2);
+}
