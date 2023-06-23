4e3d53b513cf30d55a2738d9b31d419a8f4265a9
Gospel116 <dualedge4rock@gmail.com>
2023-06-22 19:01:39 -0700

    print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_numbers - This checks for a digit from 0 through 9
+ *
+ * Return: ALways 0
+ */
+
+void print_numbers(void)
+{
+	int c;
+
+	for (c = 48; c < 58; c++)
+	{
+		_putchar(c);
+	}
+
+	_putchar('\n');
+}
