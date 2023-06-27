208aebbd5efa117bb9d60effbddc916eea7ba42a
Gospel116 <dualedge4rock@gmail.com>
2023-06-26 21:03:12 -0700

    reset to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - This program takes a point to an integer as parameters
+ * @n: The value to updated to 98
+ * Return: ALways 0
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
